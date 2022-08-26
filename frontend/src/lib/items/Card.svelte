ba0bbeee5e439446d47baebf34df67f74faedecb
Nikita Galaiko <nikita@galaiko.rocks>
2022-08-26 09:12:15 +0200

    frontend: display item chance

--- frontend/src/lib/items/Card.svelte
+++ frontend/src/lib/items/Card.svelte
@@ -4,12 +4,16 @@
 	import { Distance } from '$lib/time';
 
 	export let item: Item & { id: string };
+	export let chance: number;
 	const user = useUser(item.userId);
 </script>
 
 <div id={item.id} class="border-2 px-2 py-1 rounded-2xl">
-	<span class="font-semibold text-lg whitespace-nowrap text-ellipsis overflow-hidden">
-		{item.text}
+	<span
+		class="font-semibold text-lg whitespace-nowrap text-ellipsis overflow-hidden flex justify-between"
+	>
+		<p>{item.text}</p>
+		<p>{(chance * 100).toFixed(0)}%</p>
 	</span>
 
 	<div class="flex gap-1 opacity-50 text-sm">
--- frontend/src/lib/items/List.svelte
+++ frontend/src/lib/items/List.svelte
@@ -1,19 +1,27 @@
 <script lang="ts">
 	import { Form, Card, useItems } from '$lib/items';
-	import { compareDesc } from 'date-fns';
+	import { usePicks } from '$lib/picks';
+	import { getWeights } from '@eligo/protocol';
 
 	export let listId: string;
 	const items = useItems({ listId });
+	const picks = usePicks({ listId });
+	$: weights = getWeights($items.list, $picks.list);
+	$: weightsSum = Object.values(weights).reduce((a, b) => a + b, 0);
+	$: chances = Object.fromEntries(
+		Object.entries(weights).map(([itemId, weight]) => [itemId, weight / weightsSum])
+	);
 </script>
 
 <ul class="grid grid-cols-1 gap-2">
 	<li>
 		<Form {listId} />
 	</li>
-	{#each $items.list.sort((a, b) => compareDesc(a.createTime, b.createTime)) as item}
+	{#each $items.list.sort((a, b) => chances[b.id] - chances[a.id]) as item}
+		{@const chance = chances[item.id]}
 		{#if item.isLoading === false}
 			<li>
-				<Card {item} />
+				<Card {item} {chance} />
 			</li>
 		{/if}
 	{/each}

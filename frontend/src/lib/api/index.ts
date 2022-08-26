4d1459b2af02826e91343f9ef1ceec9e9f88e0f2
Nikita Galaiko <nikita@galaiko.rocks>
2022-08-26 08:35:27 +0200

    frontend: use new api domain

--- frontend/src/lib/api/index.ts
+++ frontend/src/lib/api/index.ts
@@ -1,6 +1,6 @@
 const host =
 	process.env.NODE_ENV === 'production'
-		? 'https://eligo-backend.fly.dev/'
+		? 'https://api.eligo.galaiko.rocks/'
 		: 'http://127.0.0.1:31337/';
 
 export const httpHost = host;

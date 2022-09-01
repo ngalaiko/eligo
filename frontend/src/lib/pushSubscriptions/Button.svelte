104741973c6e4675d37bdc9861c51c59e25afecf
Nikita Galaiko <nikita@galaiko.rocks>
2022-09-01 16:09:50 +0200

    frontend: loading state for notificaton button

--- frontend/src/lib/pushSubscriptions/Button.svelte
+++ frontend/src/lib/pushSubscriptions/Button.svelte
@@ -2,43 +2,70 @@
 	import { createSubscription, removeSubscription } from '$lib/api';
 	import { onMount } from 'svelte';
 
-	let setSubscription: (subscription: PushSubscription | null) => void;
-	let subscription = new Promise<PushSubscription | null>((resolve) => (setSubscription = resolve));
+	let isSupported = false;
+	let isEnabled = false;
+	let isSubscribing = false;
+	let isUnsubscribing = false;
 
-	const subscribe = () =>
-		navigator.serviceWorker
-			.getRegistration()
-			.then((registration) => {
-				subscription = registration.pushManager.subscribe({
-					userVisibleOnly: true,
-					applicationServerKey:
-						'BOf5qTvP_zovZipWAEL9lKsiGJC7nMs6qeTIvWoef05EQdSpGksLXCwVJ147qbAM4DO9tOrs8dAQEkQJCxXV0kc'
-				});
-				return subscription;
+	const subscribe = async () => {
+		isSubscribing = true;
+		return Notification.requestPermission()
+			.then(async (result) => {
+				if (result !== 'granted') return;
+				return navigator.serviceWorker
+					.getRegistration()
+					.then((registration) =>
+						registration.pushManager.subscribe({
+							userVisibleOnly: true,
+							applicationServerKey:
+								'BOf5qTvP_zovZipWAEL9lKsiGJC7nMs6qeTIvWoef05EQdSpGksLXCwVJ147qbAM4DO9tOrs8dAQEkQJCxXV0kc'
+						})
+					)
+					.then(createSubscription);
 			})
-			.then(createSubscription)
+			.finally(() => ((isEnabled = true), (isSubscribing = false)))
 			.catch(console.error);
+	};
 
-	const unsubscribe = (s: PushSubscription) =>
-		Promise.all([
-			removeSubscription(s),
-			s.unsubscribe().then(() => (subscription = Promise.resolve(null)))
-		]).catch(console.error);
-
-	onMount(() =>
-		navigator.serviceWorker
+	const unsubscribe = async () => {
+		isUnsubscribing = true;
+		return navigator.serviceWorker
 			.getRegistration()
 			.then((registration) => registration.pushManager.getSubscription())
-			.then(setSubscription)
+			.then((subscription) =>
+				Promise.all([
+					removeSubscription(subscription),
+					subscription.unsubscribe().then(() => (isEnabled = false))
+				])
+					.finally(() => (isUnsubscribing = false))
+					.catch(console.error)
+			);
+	};
+
+	onMount(() =>
+		navigator.serviceWorker.getRegistration().then(async (registration) => {
+			if (registration.pushManager) {
+				isSupported = true;
+				return registration.pushManager
+					.getSubscription()
+					.then((subscription) => (isEnabled = !!subscription));
+			} else {
+				isSupported = false;
+			}
+		})
 	);
 </script>
 
-{#await subscription}
-	loading...
-{:then subscription}
-	{#if subscription}
-		<button class="underline" on:click={() => unsubscribe(subscription)}>unsubscribe</button>
+{#if isSupported}
+	{#if isSubscribing}
+		<span>enabling...</span>
+	{:else if isUnsubscribing}
+		<span>disabling...</span>
+	{:else if isEnabled}
+		<button class="underline" on:click={() => unsubscribe()}>disable</button>
 	{:else}
-		<button class="underline" on:click={() => subscribe()}>subscribe</button>
+		<button class="underline" on:click={() => subscribe()}>enable</button>
 	{/if}
-{/await}
+{:else}
+	not supported
+{/if}

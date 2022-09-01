c5cb4677a55b5def8fa655e061aafeb9cb4670a2
Nikita Galaiko <nikita@galaiko.rocks>
2022-09-01 15:29:44 +0200

    fronted: display push notifications

--- frontend/src/service-worker/index.ts
+++ frontend/src/service-worker/index.ts
@@ -1,1 +1,7 @@
+import type { WebNotification } from '@eligo/protocol';
 
+self.addEventListener('push', (event: PushEvent) => {
+	const data = event.data.json() as WebNotification;
+	// @ts-ignore
+	self.registration.showNotification(data.title, data.options);
+});

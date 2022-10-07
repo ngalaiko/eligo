d8fab2f539d59563912e252bc891a37a38a135d2
Nikita Galaiko <nikita@galaiko.rocks>
2022-10-07 15:34:51 +0200

    backend: split dev and prod cors

--- backend/src/cors.ts
+++ backend/src/cors.ts
@@ -5,14 +5,18 @@ export default {
     credentials: true,
     allowedHeaders: ['Content-Type'],
     maxAge: 86400,
-    origin: [
-        'http://127.0.0.1:5173',
-        'http://127.0.0.1:4173',
-        'http://localhost:5173',
-        'http://localhost:4173',
-        'https://eligo-six.vercel.app',
-        /https:\/\/eligo(-.+)?-ngalaiko.vercel.app/,
-        'https://eligo.galaiko.rocks',
-        'https://eligo.rocks'
-    ]
+    origin:
+        process.env.NODE_ENV === 'production'
+            ? [
+                'https://eligo-six.vercel.app',
+                /https:\/\/eligo(-.+)?-ngalaiko.vercel.app/,
+                'https://eligo.galaiko.rocks',
+                'https://eligo.rocks'
+            ]
+            : [
+                'http://127.0.0.1:5173',
+                'http://127.0.0.1:4173',
+                'http://localhost:5173',
+                'http://localhost:4173'
+            ]
 } as CorsOptions;

1225105a35014a19b6d87187f6ae87d5a026df44
Nikita Galaiko <nikita@galaiko.rocks>
2022-10-14 15:25:43 +0200

    backend: format

--- backend/src/cors.ts
+++ backend/src/cors.ts
@@ -1,22 +1,22 @@
 import type { CorsOptions } from 'cors';
 
 export default {
-    methods: ['POST', 'PATCH', 'DELETE'],
-    credentials: true,
-    allowedHeaders: ['Content-Type'],
-    maxAge: 86400,
-    origin:
-        process.env.NODE_ENV === 'production'
-            ? [
-                'https://eligo-six.vercel.app',
-                /https:\/\/eligo(-.+)?-ngalaiko.vercel.app/,
-                'https://eligo.galaiko.rocks',
-                'https://eligo.rocks'
-            ]
-            : [
-                'http://127.0.0.1:5173',
-                'http://127.0.0.1:4173',
-                'http://localhost:5173',
-                'http://localhost:4173'
-            ]
+	methods: ['POST', 'PATCH', 'DELETE'],
+	credentials: true,
+	allowedHeaders: ['Content-Type'],
+	maxAge: 86400,
+	origin:
+		process.env.NODE_ENV === 'production'
+			? [
+					'https://eligo-six.vercel.app',
+					/https:\/\/eligo(-.+)?-ngalaiko.vercel.app/,
+					'https://eligo.galaiko.rocks',
+					'https://eligo.rocks'
+			  ]
+			: [
+					'http://127.0.0.1:5173',
+					'http://127.0.0.1:4173',
+					'http://localhost:5173',
+					'http://localhost:4173'
+			  ]
 } as CorsOptions;
--- backend/src/random.ts
+++ backend/src/random.ts
@@ -1,31 +1,31 @@
 export default () => {
-    const queue: number[] = [];
+	const queue: number[] = [];
 
-    let fetching: Promise<void> | null = null;
-    const populateQueue = async () => {
-        if (!fetching) {
-            fetching =
-                // returns 1024 numbers from 0 to 65535 generatrd by a quantum computer
-                // reference: https://qrng.anu.edu.au/contact/api-documentation/
-                fetch(`https://qrng.anu.edu.au/API/jsonI.php?length=1024&type=uint16`)
-                    .then((r) => r.json())
-                    .catch((e) => {
-                        console.error(e);
-                        return { data: [] };
-                    })
-                    .then(({ data }: { data?: number[] }) => data?.map((d: number) => d / 65535))
-                    .then((numbers) => numbers?.forEach((n) => queue.push(n)));
-        }
-        await fetching;
-        fetching = null;
-    };
+	let fetching: Promise<void> | null = null;
+	const populateQueue = async () => {
+		if (!fetching) {
+			fetching =
+				// returns 1024 numbers from 0 to 65535 generatrd by a quantum computer
+				// reference: https://qrng.anu.edu.au/contact/api-documentation/
+				fetch(`https://qrng.anu.edu.au/API/jsonI.php?length=1024&type=uint16`)
+					.then((r) => r.json())
+					.catch((e) => {
+						console.error(e);
+						return { data: [] };
+					})
+					.then(({ data }: { data?: number[] }) => data?.map((d: number) => d / 65535))
+					.then((numbers) => numbers?.forEach((n) => queue.push(n)));
+		}
+		await fetching;
+		fetching = null;
+	};
 
-    populateQueue();
+	populateQueue();
 
-    return {
-        next: () => {
-            if (queue.length < 100) populateQueue();
-            return queue.shift() ?? Math.random();
-        }
-    };
+	return {
+		next: () => {
+			if (queue.length < 100) populateQueue();
+			return queue.shift() ?? Math.random();
+		}
+	};
 };

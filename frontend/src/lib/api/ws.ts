0fa3a1d39ad0f965ff6899673e11f67a0049b805
Nikita Galaiko <nikita@galaiko.rocks>
2022-10-21 09:05:35 +0200

    frontend: store raw actions log

--- frontend/src/lib/api/ws.ts
+++ frontend/src/lib/api/ws.ts
@@ -1,90 +1,89 @@
 import io from 'socket.io-client';
 import { wsHost } from './http';
 import { dev } from '$app/environment';
-import { writable, type Writable, type Readable } from 'svelte/store';
+import { writable, derived } from 'svelte/store';
 import {
-	lists,
-	reduce,
-	emptyState,
-	type Action,
-	users,
-	picks,
-	items,
-	boosts,
-	memberships,
-	webPushSuscriptions
+    type Action,
+    emptyState,
+    reduce,
+    lists,
+    users,
+    picks,
+    items,
+    boosts,
+    memberships,
+    webPushSuscriptions
 } from '@eligo/state';
 import type { Error } from '@eligo/protocol';
 
-type User = { id: string; name: string };
-
-const stateEvents = [
-	lists.created.type,
-	lists.updated.type,
-	lists.deleted.type,
-
-	users.created.type,
-	users.updated.type,
-	users.deleted.type,
-
-	picks.created.type,
-	picks.updated.type,
-	picks.deleted.type,
-
-	items.created.type,
-	items.updated.type,
-	items.deleted.type,
-
-	boosts.created.type,
-	boosts.updated.type,
-	boosts.deleted.type,
-
-	memberships.created.type,
-	memberships.updated.type,
-	memberships.deleted.type,
-
-	webPushSuscriptions.created.type,
-	webPushSuscriptions.updated.type,
-	webPushSuscriptions.deleted.type
-];
-
-const toReadable = <T>(store: Writable<T>): Readable<T> => ({
-	subscribe: store.subscribe
-});
-
-export const auth = writable<{ user?: User }>({});
-const stateStore = writable(emptyState);
-export const state = toReadable(stateStore);
+const actions = writable([] as Action[]);
+export const state = derived(actions, (actions) => actions.reduce(reduce, emptyState));
 
 const socket = io(wsHost, {
-	withCredentials: true
+    withCredentials: true
 });
 
 export const send = async (action: Action) =>
-	new Promise<void>((resolve, reject) =>
-		socket.emit(action.type, action.payload, (error: Error) => {
-			if (error) {
-				reject(new Error(error.message));
-			} else {
-				resolve();
-			}
-		})
-	);
+    new Promise<void>((resolve, reject) =>
+        socket.emit(action.type, action.payload, (error: Error) => {
+            if (error) {
+                reject(new Error(error.message));
+            } else {
+                resolve();
+            }
+        })
+    );
 
-if (dev) socket.onAny((event, ...args) => console.debug(event, args));
+// auth
+
+type User = { id: string; name: string };
+
+export const auth = writable<{ user?: User }>({});
 
 socket.on('auth', (user: User) => {
-	stateStore.set(emptyState);
-	auth.set({ user });
+    actions.set([]);
+    auth.set({ user });
 });
 
 auth.subscribe(({ user }) => {
-	if (user && socket.disconnected) socket.connect();
-	if (!user && socket.connected) socket.disconnect();
+    if (user && socket.disconnected) socket.connect();
+    if (!user && socket.connected) socket.disconnect();
 });
 
-stateEvents.forEach((eventType) =>
-	socket.on(eventType, (action) =>
-		stateStore.update((stateStore) => reduce(stateStore, { type: eventType, payload: action }))
-	)
+// debug logs
+if (dev) socket.onAny((event, ...args) => console.debug(event, args));
+
+// subscribe to events
+[
+    lists.created.type,
+    lists.updated.type,
+    lists.deleted.type,
+
+    users.created.type,
+    users.updated.type,
+    users.deleted.type,
+
+    picks.created.type,
+    picks.updated.type,
+    picks.deleted.type,
+
+    items.created.type,
+    items.updated.type,
+    items.deleted.type,
+
+    boosts.created.type,
+    boosts.updated.type,
+    boosts.deleted.type,
+
+    memberships.created.type,
+    memberships.updated.type,
+    memberships.deleted.type,
+
+    webPushSuscriptions.created.type,
+    webPushSuscriptions.updated.type,
+    webPushSuscriptions.deleted.type
+].forEach((eventType) =>
+    socket.on(eventType, (action) =>
+        actions.update((log) => [...log, { type: eventType, payload: action }])
+    )
 );
--- frontend/src/routes/lists/[id]/+layout.ts
+++ frontend/src/routes/lists/[id]/+layout.ts
@@ -1,5 +1,7 @@
 import type { LayoutLoad } from './$types';
 
 export const load: LayoutLoad = ({ params }) => ({
-	listId: params.id
+    listId: params.id
 });
+
+export const prerender = false;

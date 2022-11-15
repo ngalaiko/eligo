0c6a72f7433506f9d97a5a1f833dd782e0b92690
Nikita Galaiko <nikita@galaiko.rocks>
2022-11-15 09:10:45 +0100

    frontend: add badge to web push

--- frontend/src/service-worker/index.ts
+++ frontend/src/service-worker/index.ts
@@ -1,10 +1,11 @@
 import type { WebNotification } from '@eligo/protocol';
 
 self.addEventListener('push', (event: PushEvent) => {
-	const data = event.data.json() as WebNotification;
-	// @ts-ignore
-	self.registration.showNotification(data.title, {
-		...data.options,
-		icon: '/android-chrome-192x192.png'
-	});
+    const { title, options } = event.data.json() as WebNotification;
+    // @ts-ignore
+    self.registration.showNotification(title, {
+        ...options,
+        icon: '/android-chrome-192x192.png',
+        badge: '/favicon-32x32.png'
+    });
 });

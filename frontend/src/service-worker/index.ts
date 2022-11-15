84e8edde639f84c9f7b874ea8aa6f7ca04fdf300
Nikita Galaiko <nikita@galaiko.rocks>
2022-11-15 15:37:38 +0100

    frontend: fix typing in service worker

--- frontend/src/service-worker/index.ts
+++ frontend/src/service-worker/index.ts
@@ -1,9 +1,11 @@
 import type { WebNotification } from '@eligo/protocol';
 
+const worker = self as unknown as ServiceWorkerGlobalScope;
+
+// display push notifications
 self.addEventListener('push', (event: PushEvent) => {
     const { title, options } = event.data.json() as WebNotification;
-    // @ts-ignore
-    self.registration.showNotification(title, {
+    worker.registration.showNotification(title, {
         ...options,
         icon: '/android-chrome-192x192.png',
         badge: '/favicon-32x32.png'

7a9f76beee0e657c2238f7222a18ca8fb2b411e7
Nikita Galaiko <nikita@galaiko.rocks>
2022-11-15 09:10:22 +0100

    {protocol,backend}: push notification timestamp

--- protocol/src/types.ts
+++ protocol/src/types.ts
@@ -1,66 +1,67 @@
 export type Error = {
-	code: string;
-	message: string;
+    code: string;
+    message: string;
 };
 
 export type WebNotification = {
-	title: string;
-	options: {
-		body: string;
-	};
+    title: string;
+    options: {
+        body: string;
+        timestamp: number;
+    };
 };
 
 type Type<T extends any> = T & {
-	id: string;
-	createTime: EpochTimeStamp;
-	updateTime?: EpochTimeStamp;
-	deleteTime?: EpochTimeStamp;
+    id: string;
+    createTime: EpochTimeStamp;
+    updateTime?: EpochTimeStamp;
+    deleteTime?: EpochTimeStamp;
 };
 
 export type WebPushSubscription = Type<{
-	id: string;
-	endpoint: string;
-	userId: string;
-	expirationTime: EpochTimeStamp | null;
-	keys: { p256dh: string; auth: string };
+    id: string;
+    endpoint: string;
+    userId: string;
+    expirationTime: EpochTimeStamp | null;
+    keys: { p256dh: string; auth: string };
 }>;
 
 export type JWTPublicKey = Type<{
-	spki: string;
-	alg: string;
+    spki: string;
+    alg: string;
 }>;
 
 export type User = Type<{
-	name: string;
-	displayName?: string;
-	hash?: string;
+    name: string;
+    displayName?: string;
+    hash?: string;
 }>;
 
 export type List = Type<{
-	title: string;
-	userId: string;
-	invitatationId?: string;
+    title: string;
+    userId: string;
+    invitatationId?: string;
 }>;
 
 export type Membership = Type<{
-	listId: string;
-	userId: string;
+    listId: string;
+    userId: string;
 }>;
 
 export type Item = Type<{
-	text: string;
-	listId: string;
-	userId: string;
+    text: string;
+    listId: string;
+    userId: string;
 }>;
 
 export type Pick = Type<{
-	listId: string;
-	userId: string;
-	itemId?: string;
+    listId: string;
+    userId: string;
+    itemId?: string;
 }>;
 
 export type Boost = Type<{
-	userId: string;
-	listId: string;
-	itemId: string;
+    userId: string;
+    listId: string;
+    itemId: string;
 }>;

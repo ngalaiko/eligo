bdedcf4ef9021d7508ae46adfa1beb5e2dc6a713
Nikita Galaiko <nikita@galaiko.rocks>
2022-11-15 15:22:04 +0100

    frontend,backend: serve https

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,34 +1,35 @@
 import yargs from 'yargs';
 
 const argv = yargs(process.argv.slice(2))
-	.usage('Usage: $0 [options]')
-	.option('database', {
-		alias: 'd',
-		describe: 'Database path',
-		default: './database.dev.jsonl'
-	})
-	.option('port', {
-		alias: 'p',
-		describe: 'Port to listen on',
-		default: 31337
-	})
-	.option('host', {
-		alias: 'h',
-		describe: 'Host to listen on',
-		default: '127.0.0.1'
-	})
-	.option('vapid-public-key', {
-		describe: 'Public VAPID key to send WebPush notifications',
-		default:
-			'BOf5qTvP_zovZipWAEL9lKsiGJC7nMs6qeTIvWoef05EQdSpGksLXCwVJ147qbAM4DO9tOrs8dAQEkQJCxXV0kc'
-	})
-	.option('vapid-private-key-path', {
-		describe: 'Path to a file with VALID private key to send WebPush notifications',
-		default: './vapid-private-key.txt'
-	})
-	.parseSync();
+    .usage('Usage: $0 [options]')
+    .option('database', {
+        alias: 'd',
+        describe: 'Database path',
+        default: './database.dev.jsonl'
+    })
+    .option('port', {
+        alias: 'p',
+        describe: 'Port to listen on',
+        default: 31337
+    })
+    .option('host', {
+        alias: 'h',
+        describe: 'Host to listen on',
+        default: 'localhost'
+    })
+    .option('vapid-public-key', {
+        describe: 'Public VAPID key to send WebPush notifications',
+        default:
+            'BOf5qTvP_zovZipWAEL9lKsiGJC7nMs6qeTIvWoef05EQdSpGksLXCwVJ147qbAM4DO9tOrs8dAQEkQJCxXV0kc'
+    })
+    .option('vapid-private-key-path', {
+        describe: 'Path to a file with VALID private key to send WebPush notifications',
+        default: './vapid-private-key.txt'
+    })
+    .parseSync();
 
 import { createServer } from 'http';
+import { createServer as httpsCreateServer } from 'https';
 import openDatabase from './db.js';
 import createNotifications from './notifications.js';
 import setupTokens from './tokens.js';
@@ -39,22 +40,36 @@ import cors from 'cors';
 import corsOptions from './cors.js';
 import { Server } from 'socket.io';
 import { readFileSync } from 'fs';
+import { dirname } from 'path';
 
 const database = openDatabase(argv.database);
 const tokens = await setupTokens(database);
 const notifications = createNotifications(
-	{
-		subject: 'mailto:nikita@galaiko.rocks',
-		privateKey: readFileSync(argv.vapidPrivateKeyPath).toString().trim(),
-		publicKey: argv.vapidPublicKey
-	},
-	database
+    {
+        subject: 'mailto:nikita@galaiko.rocks',
+        privateKey: readFileSync(argv.vapidPrivateKeyPath).toString().trim(),
+        publicKey: argv.vapidPublicKey
+    },
+    database
 );
 
-const server = createServer();
+const isDevelopment = process.env.NODE_ENV === 'development';
+
+const pwd = dirname(process.argv[1]);
+
+const server = isDevelopment
+    ? httpsCreateServer({
+        cert: readFileSync(`${pwd}/../.cert/localhost.pem`),
+        key: readFileSync(`${pwd}/../.cert/localhost-key.pem`)
+    })
+    : createServer();
+
 const app = polka({ server })
-	.use(cors(corsOptions))
-	.listen(argv.port, argv.host, () => console.log(`listening on ${argv.host}:${argv.port}`));
+    .use(cors(corsOptions))
+    .listen(argv.port, argv.host, () =>
+        console.log(`listening on ${isDevelopment ? 'https' : 'http'}://${argv.host}:${argv.port}`)
+    );
+
 const io = new Server(server, { cors: corsOptions });
 
 setupHttp(app, database, tokens, io, notifications);
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,10 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import mkcert from 'vite-plugin-mkcert';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()]
+    server: { https: true },
+    plugins: [sveltekit(), mkcert()]
 };
 
 export default config;

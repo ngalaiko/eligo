ab4dda32afe8a933168c23de532d24f0565078cf
Nikita Galaiko <nikita@galaiko.rocks>
2022-11-30 12:51:24 +0100

    backend: send notifications when subscribed

--- backend/src/notifications.ts
+++ backend/src/notifications.ts
@@ -1,12 +1,13 @@
 // @ts-ignore
 import webpush from 'web-push';
-import type { WebNotification } from '@eligo/protocol';
+import type { WebNotification, WebPushSubscription } from '@eligo/protocol';
 import type { Database } from './db.js';
 import { webPushSuscriptions } from '@eligo/state';
 import { Server } from 'socket.io';
 
 export type Notifications = {
 	notify: (userId: string, notification: WebNotification) => void;
+	send: (subscription: WebPushSubscription, notification: WebNotification) => void;
 };
 
 export default (
@@ -17,28 +18,34 @@ export default (
 	},
 	database: Database,
 	io: Server
-) => ({
-	notify: async (userId: string, notification: WebNotification) =>
-		database.filter('webPushSuscriptions', { userId }).then((subscriptions) =>
-			subscriptions.forEach((subscription) => {
-				webpush
-					.sendNotification(subscription, JSON.stringify(notification), {
-						vapidDetails
-					})
-					.then(() => console.info(`${notification} sent to ${userId}`))
-					.catch((error) => {
-						if (error.statusCode === 410) {
-							const deleted = webPushSuscriptions.deleted({
-								id: subscription.id,
-								deleteTime: new Date().getTime()
-							});
-
-							database.append(userId, deleted);
-							io.to(deleted.payload.id).emit(deleted.type, deleted.payload);
-						} else {
-							console.error(error);
-						}
-					});
+) => {
+	const send = async (subscription: WebPushSubscription, notification: WebNotification) =>
+		webpush
+			.sendNotification(subscription, JSON.stringify(notification), {
+				vapidDetails
 			})
-		)
-});
+			.then(() => console.info(`${notification} sent to ${subscription.userId}`))
+			.catch((error) => {
+				if (error.statusCode === 410) {
+					const deleted = webPushSuscriptions.deleted({
+						id: subscription.id,
+						deleteTime: new Date().getTime()
+					});
+
+					database.append(subscription.userId, deleted);
+					io.to(deleted.payload.id).emit(deleted.type, deleted.payload);
+				} else {
+					console.error(error);
+				}
+			});
+
+	return {
+		send,
+		notify: async (userId: string, notification: WebNotification) =>
+			database
+				.filter('webPushSuscriptions', { userId })
+				.then((subscriptions) =>
+					subscriptions.forEach((subscription) => send(subscription, notification))
+				)
+	};
+};
--- backend/src/ws/webPushSubscriptions.ts
+++ backend/src/ws/webPushSubscriptions.ts
@@ -1,8 +1,9 @@
 import { webPushSuscriptions } from '@eligo/state';
 import type { Server, Socket } from 'socket.io';
 import type { Database } from '../db.js';
+import { Notifications } from '../notifications.js';
 
-export default (io: Server, socket: Socket, database: Database) => {
+export default (io: Server, socket: Socket, database: Database, notifications: Notifications) => {
 	socket.on(
 		webPushSuscriptions.create.type,
 		async (req: ReturnType<typeof webPushSuscriptions.create>['payload'], callback) => {
@@ -11,6 +12,14 @@ export default (io: Server, socket: Socket, database: Database) => {
 
 			socket.join(created.payload.id);
 			io.to(created.payload.id).emit(created.type, created.payload);
+
+			notifications.send(req, {
+				title: 'Notifications enabled',
+				options: {
+					body: 'You will receive notifications now',
+					timestamp: new Date().getTime()
+				}
+			});
 
 			callback(null);
 		}

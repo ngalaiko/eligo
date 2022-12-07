13e82f5cd8c09fa7dd6bdcf78b9dc848d1511d2d
Nikita Galaiko <nikita@galaiko.rocks>
2022-12-07 10:27:17 +0100

    backend: add GET /auth

--- backend/src/http/auth.ts
+++ backend/src/http/auth.ts
@@ -11,76 +11,97 @@ import { compare } from 'bcrypt';
 export const authCookieName = 'token';
 
 export default (database: Database, tokens: Tokens) => ({
-	middleware: async (req: Request, res: Response, next: NextHandler) => {
-		if (req.headers.cookie === undefined) {
-			next();
-			return;
-		}
+    middleware: async (req: Request, res: Response, next: NextHandler) => {
+        if (req.headers.cookie === undefined) {
+            next();
+            return;
+        }
 
-		const token = parseCookie(req.headers.cookie)[authCookieName];
-		if (token === undefined) {
-			next();
-			return;
-		}
+        const token = parseCookie(req.headers.cookie)[authCookieName];
+        if (token === undefined) {
+            next();
+            return;
+        }
 
-		try {
-			const { payload } = await tokens.verify(token);
-			req.userId = payload.sub;
-			next();
-		} catch (err) {
-			res.statusCode = 401;
-			res.end();
-		}
-	},
+        try {
+            const { payload } = await tokens.verify(token);
+            req.userId = payload.sub;
+            next();
+        } catch (err) {
+            res.statusCode = 401;
+            res.end();
+        }
+    },
 
-	handler: polka()
-		.delete('/', async (_req, res) => {
-			const cookie = serialize(authCookieName, '', {
-				httpOnly: true,
-				expires: addDays(new Date(), -1)
-			});
-			res.setHeader('Set-Cookie', cookie);
-			res.statusCode = 204;
-			res.end();
-		})
-		.post('/', async (req, res) => {
-			try {
-				const { name, password } = req.body;
-				if (name === 'undefined') throw new HTTPError(400, errRequired(`'name' is required`));
-				if (typeof name !== 'string')
-					throw new HTTPError(400, errInvalid(`'name' must be a string`));
-				if (name.length === 0) throw new HTTPError(400, errEmpty(`'name' can not be empty`));
-				if (password === 'undefined')
-					throw new HTTPError(400, errRequired(`'pasword' is required`));
-				if (typeof password !== 'string')
-					throw new HTTPError(400, errInvalid(`'password' must be a string`));
-				if (password.length === 0)
-					throw new HTTPError(400, errEmpty(`'password' can not be empty`));
+    handler: polka()
+        .get('/', async (req: Request, res) => {
+            try {
+                if (req.userId === undefined) throw new HTTPError(404, errNotFound('Not found.'));
+                const user = await database.find('users', { id: req.userId });
 
-				const user = await database.find('users', { name });
-				if (!user) throw new HTTPError(404, errNotFound('User does not exist'));
+                if (!user) throw new HTTPError(404, errNotFound('Not found.'));
 
-				const isPasswordCorrect = await compare(password, user.hash!);
-				if (!isPasswordCorrect) throw new HTTPError(403, errInvalid('Wrong password'));
+                res.setHeader('Content-Type', 'application/json');
+                res.end(JSON.stringify({ ...user, hash: undefined }));
+            } catch (err) {
+                if (err instanceof HTTPError) {
+                    res.statusCode = err.status;
+                    res.setHeader('Content-Type', 'application/json');
+                    res.end(JSON.stringify(err.apiError));
+                } else {
+                    console.error(err);
+                    res.statusCode = 500;
+                    res.end('Internal server error');
+                }
+            }
+        })
+        .delete('/', async (_req, res) => {
+            const cookie = serialize(authCookieName, '', {
+                httpOnly: true,
+                expires: addDays(new Date(), -1)
+            });
+            res.setHeader('Set-Cookie', cookie);
+            res.statusCode = 204;
+            res.end();
+        })
+        .post('/', async (req, res) => {
+            try {
+                const { name, password } = req.body;
+                if (name === 'undefined') throw new HTTPError(400, errRequired(`'name' is required`));
+                if (typeof name !== 'string')
+                    throw new HTTPError(400, errInvalid(`'name' must be a string`));
+                if (name.length === 0) throw new HTTPError(400, errEmpty(`'name' can not be empty`));
+                if (password === 'undefined')
+                    throw new HTTPError(400, errRequired(`'pasword' is required`));
+                if (typeof password !== 'string')
+                    throw new HTTPError(400, errInvalid(`'password' must be a string`));
+                if (password.length === 0)
+                    throw new HTTPError(400, errEmpty(`'password' can not be empty`));
 
-				const token = await tokens.sign({ sub: user.id });
-				const cookie = serialize(authCookieName, token, {
-					httpOnly: true,
-					expires: addDays(new Date(), 28)
-				});
-				res.setHeader('Set-Cookie', cookie);
-				res.setHeader('Content-Type', 'application/json');
-				res.end(JSON.stringify({ id: user.id, name: user.name }));
-			} catch (err) {
-				if (err instanceof HTTPError) {
-					res.statusCode = err.status;
-					res.setHeader('Content-Type', 'application/json');
-					res.end(JSON.stringify(err.apiError));
-				} else {
-					console.error(err);
-					res.statusCode = 500;
-					res.end('Internal server error');
-				}
-			}
-		})
+                const user = await database.find('users', { name });
+                if (!user) throw new HTTPError(404, errNotFound('User does not exist'));
+
+                const isPasswordCorrect = await compare(password, user.hash!);
+                if (!isPasswordCorrect) throw new HTTPError(403, errInvalid('Wrong password'));
+
+                const token = await tokens.sign({ sub: user.id });
+                const cookie = serialize(authCookieName, token, {
+                    httpOnly: true,
+                    expires: addDays(new Date(), 28)
+                });
+                res.setHeader('Set-Cookie', cookie);
+                res.setHeader('Content-Type', 'application/json');
+                res.end(JSON.stringify({ ...user, hash: undefined }));
+            } catch (err) {
+                if (err instanceof HTTPError) {
+                    res.statusCode = err.status;
+                    res.setHeader('Content-Type', 'application/json');
+                    res.end(JSON.stringify(err.apiError));
+                } else {
+                    console.error(err);
+                    res.statusCode = 500;
+                    res.end('Internal server error');
+                }
+            }
+        })
 });

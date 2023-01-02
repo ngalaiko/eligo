ec9b2e8f55c4d677549f0b81a10cd0cc625e459e
Nikita Galaiko <nikita@galaiko.rocks>
2023-01-02 10:00:47 +0100

    app: add html-minifier

--- app/src/hooks.server.ts
+++ app/src/hooks.server.ts
@@ -1,20 +1,49 @@
 import type { Handle } from '@sveltejs/kit';
 import { signer, verifier, openDatabase } from '@eligo/server';
 import { building, dev } from '$app/environment';
+import { minify, type Options as HtmlMinifierOptions } from 'html-minifier';
+
+const minificationOptions: HtmlMinifierOptions = {
+    collapseBooleanAttributes: true,
+    collapseWhitespace: true,
+    conservativeCollapse: true,
+    decodeEntities: true,
+    html5: true,
+    ignoreCustomComments: [/^#/],
+    minifyCSS: true,
+    minifyJS: false,
+    removeAttributeQuotes: true,
+    removeComments: false, // some hydration code needs comments, so leave them in
+    removeOptionalTags: true,
+    removeRedundantAttributes: true,
+    removeScriptTypeAttributes: true,
+    removeStyleLinkTypeAttributes: true,
+    sortAttributes: true,
+    sortClassName: true
+};
 
 let database = building
-	? undefined
-	: openDatabase(dev ? 'database.dev.jsonl' : '/data/database.jsonl');
+    ? undefined
+    : openDatabase(dev ? 'database.dev.jsonl' : '/data/database.jsonl');
 
 let tokens = building
-	? undefined
-	: signer(database!).then((signer) => ({ ...signer, ...verifier(database!) }));
+    ? undefined
+    : signer(database!).then((signer) => ({ ...signer, ...verifier(database!) }));
 
 export const handle: Handle = async ({ event, resolve }) => {
-	if (!building) {
-		event.locals.database = database!;
-		event.locals.tokens = await tokens!;
-	}
+    if (!building) {
+        event.locals.database = database!;
+        event.locals.tokens = await tokens!;
+    }
 
-	return await resolve(event);
+    let page = '';
+    return resolve(event, {
+        transformPageChunk: ({ html, done }) => {
+            page += html;
+            if (done) {
+                return building ? minify(page, minificationOptions) : page;
+            }
+            return;
+        }
+    });
 };

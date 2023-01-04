cd4cd84a140a3e75d6cd726c280fb5d732651209
Nikita Galaiko <nikita@galaiko.rocks>
2023-01-04 12:22:35 +0100

    app: format

--- app/src/hooks.server.ts
+++ app/src/hooks.server.ts
@@ -4,46 +4,46 @@ import { building, dev } from '$app/environment';
 import { minify, type Options as HtmlMinifierOptions } from 'html-minifier';
 
 const minificationOptions: HtmlMinifierOptions = {
-    collapseBooleanAttributes: true,
-    collapseWhitespace: true,
-    conservativeCollapse: true,
-    decodeEntities: true,
-    html5: true,
-    ignoreCustomComments: [/^#/],
-    minifyCSS: true,
-    minifyJS: false,
-    removeAttributeQuotes: true,
-    removeComments: false, // some hydration code needs comments, so leave them in
-    removeOptionalTags: true,
-    removeRedundantAttributes: true,
-    removeScriptTypeAttributes: true,
-    removeStyleLinkTypeAttributes: true,
-    sortAttributes: true,
-    sortClassName: true
+	collapseBooleanAttributes: true,
+	collapseWhitespace: true,
+	conservativeCollapse: true,
+	decodeEntities: true,
+	html5: true,
+	ignoreCustomComments: [/^#/],
+	minifyCSS: true,
+	minifyJS: false,
+	removeAttributeQuotes: true,
+	removeComments: false, // some hydration code needs comments, so leave them in
+	removeOptionalTags: true,
+	removeRedundantAttributes: true,
+	removeScriptTypeAttributes: true,
+	removeStyleLinkTypeAttributes: true,
+	sortAttributes: true,
+	sortClassName: true
 };
 
 let database = building
-    ? undefined
-    : openDatabase(dev ? 'database.dev.jsonl' : '/data/database.jsonl');
+	? undefined
+	: openDatabase(dev ? 'database.dev.jsonl' : '/data/database.jsonl');
 
 let tokens = building
-    ? undefined
-    : signer(database!).then((signer) => ({ ...signer, ...verifier(database!) }));
+	? undefined
+	: signer(database!).then((signer) => ({ ...signer, ...verifier(database!) }));
 
 export const handle: Handle = async ({ event, resolve }) => {
-    if (!building) {
-        event.locals.database = database!;
-        event.locals.tokens = await tokens!;
-    }
+	if (!building) {
+		event.locals.database = database!;
+		event.locals.tokens = await tokens!;
+	}
 
-    let page = '';
-    return resolve(event, {
-        transformPageChunk: ({ html, done }) => {
-            page += html;
-            if (done) {
-                return building ? minify(page, minificationOptions) : page;
-            }
-            return;
-        }
-    });
+	let page = '';
+	return resolve(event, {
+		transformPageChunk: ({ html, done }) => {
+			page += html;
+			if (done) {
+				return building ? minify(page, minificationOptions) : page;
+			}
+			return;
+		}
+	});
 };
